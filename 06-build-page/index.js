5f2ab879a2a270b6e5d8a49f2546f4d2d288e912
Sanjar Tukhtamishev <94516310+sthm23@users.noreply.github.com>
2022-05-24 20:51:26 +0500

    'fix: correct if index.html not {{elements}}'

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,13 +2,21 @@ const fs = require('fs');
 const fsPromise = require('fs/promises');
 const path = require('path');
 
+fs.access(path.join(__dirname, 'project-dist'), err=>{
+    if(err){
+        const html = fs.createWriteStream(path.join(__dirname, 'project-dist', 'index.html'));
+        const tempHtml = fs.createReadStream(path.join(__dirname, 'template.html'), 'utf-8');
+        tempHtml.pipe(html);
+        htmlCreater(path.join(__dirname, 'template.html'));
+    }else{
+        htmlCreater(path.join(__dirname, 'project-dist', 'index.html'));
+    }
+});
 fs.mkdir(path.join(__dirname, 'project-dist'), {recursive:true}, err=>err);
 let wayCopyFolder = path.join(__dirname, 'project-dist', 'assets');
 fs.mkdir(wayCopyFolder, {recursive:true}, err=>err);
 
-const html = fs.createWriteStream(path.join(__dirname, 'project-dist', 'index.html'));
-const tempHtml = fs.createReadStream(path.join(__dirname, 'template.html'), 'utf-8');
-tempHtml.pipe(html);
+
 
 const css = fs.createWriteStream(path.join(__dirname, 'project-dist', 'style.css'));
 
@@ -55,31 +63,30 @@ function saveToIndex (str)  {
     let html = fs.createWriteStream(path.join(__dirname, 'project-dist', 'index.html'));
     html.write(str);
 }
-fs.readFile(path.join(__dirname, 'template.html'), 'utf-8', async (err, data)=>{
-    if(err)console.log(err);
-    let templateOrg = data;
-    let allTemplateTags = data.match(/{{(.*?)}}/g).map( function(val) {
-        return val;
+function htmlCreater(way){
+    fs.readFile(way, 'utf-8', (err, data)=>{
+        if(err)console.log(err);
+        let templateOrg = data;
+        if(!data.match(/{{(.*?)}}/g)){
+            return '';
+        }
+        let allTemplateTags = data.match(/{{(.*?)}}/g).map( function(val) {
+            return val;
+        });
+    
+        allTemplateTags.forEach( (item,index) => {
+            let component_name = item.replace('{{', '').replace('}}', '');
+    
+            fs.readFile(path.join(__dirname, 'components', `${component_name}.html` ), 'utf-8', (err, data)=>{
+                if(err)console.log(err);
+                
+                templateOrg = templateOrg.replace(item, data);
+                
+                if(allTemplateTags.length -1 === index){
+                    saveToIndex(templateOrg);
+                }
+            }); 
+        });
     });
+}
 
-    let resolve, promise = new Promise((res)=>{
-        resolve = res;
-    });
-
-    allTemplateTags.forEach( (item,index) => {
-        let component_name = item.replace('{{', '').replace('}}', '');
-
-        fs.readFile(path.join(__dirname, 'components', `${component_name}.html` ), 'utf-8', (err, data)=>{
-            if(err)console.log(err);
-            
-            templateOrg = templateOrg.replace(item, data);
-            
-            if(allTemplateTags.length -1 === index){
-                resolve(templateOrg);
-            }
-        }); 
-    });
-    promise.then(c =>{
-        saveToIndex(c);
-    });
-});
